f1053fe4324c69e9bcecbe681c12e10eb3aa99c1
Flavio Aquino <bregarulai@gmail.com>
2017-06-26 21:22:00 -0400

    created test for addVisitorToDb method

--- libraryDatabaseUtility/src/test/java/libraryDatabaseUtility/repository/TestVisitorDAOImpl.java
+++ libraryDatabaseUtility/src/test/java/libraryDatabaseUtility/repository/TestVisitorDAOImpl.java
@@ -1,22 +1,36 @@
+
 package libraryDatabaseUtility.repository;
 
 import static org.junit.Assert.*;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class TestVisitorDAOImpl {
+import libraryDatabaseUtility.model.DataSource;
+import libraryDatabaseUtility.model.Visitor;
 
+/**
+ * @author Flavio Luis
+ *
+ */
+public class TestVisitorDAOImpl {
+	VisitorDAOImpl target;
+	DataSource source;
+	Visitor visitor;
 
 
 	@Before
 	public void setUp() throws Exception {
+		target = new VisitorDAOImpl();
+		source = DataSource.getInstance();
+		visitor = new Visitor();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testVisitorAddedToDatabase() {
+		int result = 1;
+		assertEquals(target.addVisitorToDb(source, visitor), result);
+		
 	}
 
 }
