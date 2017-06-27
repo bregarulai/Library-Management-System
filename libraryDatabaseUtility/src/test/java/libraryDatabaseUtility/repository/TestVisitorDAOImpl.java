4933483a58a7dd71ee919714fa9f1e1898f2e093
Flavio Aquino <bregarulai@gmail.com>
2017-06-26 21:16:22 -0400

    created new test suite for VisitorDAOImpl

---
+++ libraryDatabaseUtility/src/test/java/libraryDatabaseUtility/repository/TestVisitorDAOImpl.java
@@ -0,0 +1,22 @@
+package libraryDatabaseUtility.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestVisitorDAOImpl {
+
+
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
