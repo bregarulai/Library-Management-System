3ace5b0ca132e16c41fa83cf199b53105254aba6
Flavio Aquino <bregarulai@gmail.com>
2017-07-10 08:33:31 -0400

    implemented addVisitorToDb method

--- libraryDatabaseUtility/src/main/java/libraryDatabaseUtility/repository/VisitorDAOImpl.java
+++ libraryDatabaseUtility/src/main/java/libraryDatabaseUtility/repository/VisitorDAOImpl.java
@@ -3,6 +3,9 @@
  */
 package libraryDatabaseUtility.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 
@@ -14,13 +17,29 @@ import libraryDatabaseUtility.model.Visitor;
  *
  */
 public class VisitorDAOImpl implements VisitorDAO {
-
+	private static final String INSERT_VISITOR_SQL = "insert into visitors (firstName, lastName) values (?, ?)";
+	
+	public VisitorDAOImpl() {
+		
+	}
+	
 	/* (non-Javadoc)
 	 * @see libraryDatabaseUtility.repository.VisitorDAO#addVisitorToDb(libraryDatabaseUtility.model.DataSource, libraryDatabaseUtility.model.Visitor)
 	 */
-	public int addVisitorToDb(DataSource source, Visitor member) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int addVisitorToDb(DataSource source, Visitor member) throws SQLException {
+		int result = 0;
+		PreparedStatement statement = null;
+		Connection connection = source.getConnection();
+		
+		statement = connection.prepareStatement(INSERT_VISITOR_SQL);
+		statement.setString(1, member.getFirstName());
+		statement.setString(2, member.getLastName());
+		
+		result = statement.executeUpdate();
+		
+		statement.close();
+		connection.close();
+		return result;
 	}
 
 	/* (non-Javadoc)
